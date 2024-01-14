4da6742af56fa6999e731bf9659926406e1da53f
Sammy Ghazavi <saham.ghazavi@gmail.com>
2024-01-14 17:58:43 -0500

    Added initial playerView, title label, autor label and detail textView

--- VideoPlayerStoryboard/View/VideoPlayerViewController.swift
+++ VideoPlayerStoryboard/View/VideoPlayerViewController.swift
@@ -3,6 +3,13 @@ import Down
 
 class VideoPlayerViewController: UIViewController {
 
+    // MARK: - Outlet
+    @IBOutlet weak var playerView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var detailTextView: UITextView!
+    
+    // MARK: - View LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
