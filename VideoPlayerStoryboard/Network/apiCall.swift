c972b18c36aab64f43def77feeb22dc3581b26ca
Sammy Ghazavi <saham.ghazavi@gmail.com>
2024-01-14 18:34:23 -0500

    Added network Call and setupPlayer

--- VideoPlayerStoryboard/Network/apiCall.swift
+++ VideoPlayerStoryboard/Network/apiCall.swift
@@ -3,15 +3,15 @@ import Foundation
 class apiCall {
     static let shared = apiCall()
     let urlString: String = constant.backendURL
-}
-func getAllVideos(from urlString:String) async throws -> [Video]?{
-    do {
-        if let url = URL(string: urlString) {
-            let (data, response) = try await URLSession.shared.data(from: url)
-            guard let httpResponse = response as? HTTPURLResponse,
-                  httpResponse.statusCode == 200 else { return nil }
-            return try JSONDecoder().decode([Video].self, from: data)
+    func getAllVideos() async throws -> [Video]?{
+        do {
+            if let url = URL(string: urlString) {
+                let (data, response) = try await URLSession.shared.data(from: url)
+                guard let httpResponse = response as? HTTPURLResponse,
+                      httpResponse.statusCode == 200 else { return nil }
+                return try JSONDecoder().decode([Video].self, from: data)
+            }
         }
+        return nil
     }
-    return nil
 }
--- VideoPlayerStoryboard/View/VideoPlayerViewController.swift
+++ VideoPlayerStoryboard/View/VideoPlayerViewController.swift
@@ -1,4 +1,6 @@
 import UIKit
+import AVFoundation
+import AVKit
 import Down
 
 class VideoPlayerViewController: UIViewController {
@@ -11,9 +13,37 @@ class VideoPlayerViewController: UIViewController {
     
     // MARK: - Variables
     var videoModel:[Video]?
-    
+    var apiManager = apiCall.shared
+    var player = AVPlayer()
+    var shouldPlay = false
     // MARK: - View LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        Task{
+            do {
+                if let model = try await apiManager.getAllVideos() {
+                    self.videoModel = model
+                }
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    func setupPlayer(forVideo video:Video?) {
+        guard let video = video else { return }
+        if let urlString = video.fullURL, let videoURL = URL(string: urlString) {
+            player = AVPlayer(url: videoURL)
+            let playerLayer = AVPlayerLayer(player: player)
+            playerLayer.frame = playerView.bounds
+            self.playerView.layer.addSublayer(playerLayer)
+            changePlayerStatus(player: self.player, play: shouldPlay)
+        }
+    }
+    func changePlayerStatus(player avPlayer:AVPlayer, play status:Bool) {
+        if status, avPlayer.status == .readyToPlay {
+            avPlayer.play()
+        } else {
+            avPlayer.pause()
+        }
     }
 }
