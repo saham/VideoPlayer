ca9230d87be7aa99e165280a3a2bf3070902a34a
Sammy Ghazavi <saham.ghazavi@gmail.com>
2024-01-14 22:22:10 -0500

    UITest file renamed

--- VideoPlayerStoryboardTests/videoPlayerStoryboardTest.swift
+++ VideoPlayerStoryboardTests/videoPlayerStoryboardTest.swift
@@ -1,13 +1,6 @@
-//
-//  test.swift
-//  VideoPlayerStoryboardTests
-//
-//  Created by Saham Ghazavi on 2024-01-14.
-//
-
 import XCTest
 
-final class test: XCTestCase {
+final class videoPlayerStoryboardTest: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
