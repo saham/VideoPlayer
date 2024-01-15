2a7a6b82f58ed49d94d38f34ba19dd55b96cc3e2
Sammy Ghazavi <saham.ghazavi@gmail.com>
2024-01-14 21:58:31 -0500

    sample Test Cases added

---
+++ VideoPlayerStoryboardTests/test.swift
@@ -0,0 +1,36 @@
+//
+//  test.swift
+//  VideoPlayerStoryboardTests
+//
+//  Created by Saham Ghazavi on 2024-01-14.
+//
+
+import XCTest
+
+final class test: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        let VideoPlayerViewController = VideoPlayerViewController()
+        let apiManager = VideoPlayerViewController.apiManager
+        XCTAssertTrue(apiManager.urlString == "http://localhost:4000/videos","apiCall points to a wrong URL")
+        XCTAssertTrue(constant.backendURL == "http://localhost:4000/videos" , "Check Backend URL")
+        XCTAssertNil(VideoPlayerViewController.videoModel,"You should start with nil video")
+       
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
